f1616d65ce58aa54e1ff19afc6a347154bac49d1
elaiten <firemann33@gmail.com>
2023-12-18 15:19:44 +0500

    Initial Commit

---
+++ SwiftUIFirst/SwiftUIFirstApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIFirstApp.swift
+//  SwiftUIFirst
+//
+//  Created by Руслан on 18.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIFirstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
